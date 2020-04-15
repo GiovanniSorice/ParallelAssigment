9254e406f7950689f3a8ce6f828a5f678fa2ec40
gs1010 <giovanni.sorice.10@gmail.com>
2020-04-15 16:25:07 +0200

    GoLSeq Class

---
+++ GoLSeq.h
@@ -0,0 +1,81 @@
+//
+// Created by gs1010 on 15/04/20.
+//
+#include <chrono>
+
+#ifndef LOF__GOLSEQ_H_
+#define LOF__GOLSEQ_H_
+
+class GoLSeq {
+ private:
+  bool *states;
+  bool *statesTmp;
+  const int nStep;
+  const int nCol;
+  const int nRow;
+ public:
+  GoLSeq(const int userStep, const int userRow, const int userCol, const int seed)
+      : nCol(userCol),
+        nRow(userRow),
+        states(new bool[userRow * userCol]{}),
+        statesTmp(new bool[userRow * userCol]{}),
+        nStep(userStep) {
+    std::srand(seed);
+    // Random initialization junping the border
+    for (int row = 1; row < nRow - 1; ++row) {
+      for (int col = 1; col < nCol - 1; ++col) {
+        states[nCol * row + col] = std::rand() % 2;
+      }
+    }
+  }
+  void PrintStates() {
+    for (int row = 0; row < nRow; ++row) {
+      for (int col = 0; col < nCol; ++col) {
+        std::cout << states[row * nCol + col] << " ";
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
+
+  void Run() {
+    int sum;
+    for (int k = 0; k < nStep; ++k) {
+      //std::cout << k << std::endl;
+      for (int row = 1; row < nRow - 1; ++row) {
+        for (int col = 1; col < nCol - 1; ++col) {
+          sum = states[(row - 1) * nCol + col - 1] +
+              states[(row - 1) * nCol + col] +
+              states[(row - 1) * nCol + col + 1] +
+              states[row * nCol + col - 1] +
+              states[row * nCol + col + 1] +
+              states[(row + 1) * nCol + col - 1] +
+              states[(row + 1) * nCol + col] +
+              states[(row + 1) * nCol + col + 1];
+
+          if (sum > 3 || sum < 2) {
+            statesTmp[row * nCol + col] = false;
+          } else {
+            statesTmp[row * nCol + col] = true;
+          }
+        }
+
+      }
+      //PrintStates();
+      bool *tmp = statesTmp;
+      statesTmp = states;
+      states = tmp;
+    }
+
+  }
+
+  void RunWithTime() {
+    auto tstart = std::chrono::high_resolution_clock::now();
+    Run();
+    auto elapsed = std::chrono::high_resolution_clock::now() - tstart;
+    auto msec = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
+    std::cout << "Spent " << msec << " msecs with " << std::endl;
+  }
+};
+
+#endif //LOF__GOLSEQ_H_
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <chrono>
+#include <omp.h>
+#include "GoLSeq.h"
 int main(int argc, char *argv[]) {
 
   if (argc == 1) {
@@ -10,65 +12,18 @@ int main(int argc, char *argv[]) {
 
   int nStep = atoi(argv[1]);
   int seed = atoi(argv[2]);
-  int nRow = atoi(argv[3]);
-  int nCol = atoi(argv[4]);
+  int const nRow = atoi(argv[3]);
+  int const nCol = atoi(argv[4]);
   int nw = atoi(argv[5]);
 
-  bool states[nRow][nCol]{};
-  bool statesTmp[nRow][nCol]{};
-
-  std::srand(seed);
-
-  // Random initialization junping the border
-  for (int row = 1; row < nRow - 1; ++row) {
-    for (int col = 1; col < nCol - 1; ++col) {
-      states[row][col] = std::rand() % 2;
-    }
-  }
-
-  for (int row = 0; row < nRow; ++row) {
-    for (int col = 0; col < nCol; ++col) {
-      std::cout << statesTmp[row][col] << " ";
-    }
-    std::cout << std::endl;
-  }
-
-  int sum = 0;
-  // Run nStep
+  GoLSeq seq(nStep, nRow, nCol, seed);
+  seq.PrintStates();
   auto tstart = std::chrono::high_resolution_clock::now();
-  for (int k = 0; k < nStep; ++k) {
-
-    std::cout << k << std::endl;
-
-    for (int row = 1; row < nRow - 1; ++row) {
-      for (int col = 1; col < nCol - 1; ++col) {
-        sum = states[row - 1][col - 1] +
-            states[row - 1][col] +
-            states[row - 1][col + 1] +
-            states[row][col - 1] +
-            states[row][col + 1] +
-            states[row + 1][col - 1] +
-            states[row + 1][col] +
-            states[row + 1][col + 1];
-
-        if (sum > 3 || sum < 2) {
-          statesTmp[row][col] = false;
-        } else {
-          statesTmp[row][col] = true;
-        }
-      }
-    }
-    for (int row = 0; row < nRow; ++row) {
-      for (int col = 0; col < nCol; ++col) {
-        std::cout << statesTmp[row][col] << " ";
-        states[row][col] = statesTmp[row][col];
-      }
-      std::cout << std::endl;
-    }
-
-  }
+  seq.Run();
   auto elapsed = std::chrono::high_resolution_clock::now() - tstart;
   auto msec = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
   std::cout << "Spent " << msec << " msecs with " << nw << " threads " << std::endl;
+  seq.RunWithTime();
+
 }
 
